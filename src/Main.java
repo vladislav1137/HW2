5e878c3aa0fede2cbfc7e6098df1643948df505e
vladislav1137 <mr.vladislav220@yandex.ru>
2023-02-03 23:45:24 +0300

    задача 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 
     public static void task1 () {
@@ -51,6 +52,18 @@ public class Main {
         System.out.println(friend);
               friend = friend / 7;
         System.out.println(friend);
-
     }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+    }
+
 }
