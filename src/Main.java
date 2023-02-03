c638ed3bf3da255375af76ccfbe125f0128507d8
vladislav1137 <mr.vladislav220@yandex.ru>
2023-02-03 23:32:47 +0300

    задача 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
     }
 
     public static void task1 () {
@@ -25,6 +27,23 @@ public class Main {
         var paper = 763789;
         paper = paper + 4;
         System.out.println(paper);
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        var dog = 8.0;
+        dog = dog - 3.5;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat - 1.6;
+        System.out.println(cat);
+        var paper = 763789;
+        paper = paper - 7639;
+        System.out.println(paper);
+    }
+
+    public static void task4() {
 
     }
 }
